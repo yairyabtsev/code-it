9369c66eef2e048cae666223223e331a3debb97a
yarrya <yairyabtsev@ispras.ru>
2021-09-18 02:35:27 +0300

    nothing

--- client/src/components/Editor/MyEditor.js
+++ client/src/components/Editor/MyEditor.js
@@ -4,18 +4,54 @@ import MonacoEditor from '@monaco-editor/react';
 import {Button} from "../Welcome/Welcome"
 
 import './MyEditor.css';
-import {Compiler} from "./Compiler"
+import {Compiler} from "./Compiler/Compiler"
 
 class MyEditor extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      // TODO: read from file!?
       code: `
+//
+// ....
+//
+
+struct location{
+  float x;
+  float y;
+};
+
+location get_position(int index);
+location get_self_position();
+
+int get_health(int index); 
+int get_self_health();
+
+void hit();
+void shoot();
+
+void turn(float angle); 
+void turn_percent(float angle) {
+  turn(angle * 3,6);
+}
+void turn_rad(float angle) {
+  angle %= 2*Pi;
+  turn(angle * 360 / 2*Pi);
+}
+
+int count_enemys(); // not a const
+
+//
+// ....
+//
+
+
 void play() {
   // put your code here;
   // it doesn't necessary to use the infinite loop,
   // because after exiting from this function -
   // it will continue to work from the entry point.
+}
 }`,
     }
     this.downloadContent = this.downloadContent.bind(this);
