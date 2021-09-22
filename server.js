bae63abbc778663d5d43761af93fea6e22f398ee
yarrya <yairyabtsev@ispras.ru>
2021-09-23 01:15:25 +0300

    findOneAndUpdate returns old data

--- server.js
+++ server.js
@@ -37,8 +37,8 @@ async function updateLocation(delta) {
     const collection = db.collection("location");
     const result = await collection.findOneAndUpdate(
       {u_id: delta.u_id}, {$inc: {x: delta.x, y: delta.y, division: delta.division}},
-      {new: true});
-    loc = result.value;//error!
+      {returnOriginal: false});
+    loc = result.value;//old data
     console.log("42");
     console.log(loc);
   } catch (err) {
@@ -53,47 +53,54 @@ async function updateLocation(delta) {
 
 async function findRoom(u_id, hash) {
   const mongoClient = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
-  let delta = {x: 0.0, y: 0.0, division: Math.random() * 100, u_id: u_id}
+  let delta = {
+    x: Math.random() * 25 + 12.5,
+    y: Math.random() * 25 + 12.5,
+    division: Math.random() * 100,
+    u_id: u_id,
+  }
   try {
     await mongoClient.connect();
     const db = mongoClient.db("codeitdb");
     const collection = db.collection("rooms");
     const result = await collection.findOneAndUpdate(
-      {capacity: {$lt: 4}}, {$inc: {capacity: 1}, $push: {players: u_id}}, {new: true});
-    let room = result.value;
+      {capacity: {$lt: 4}}, {$inc: {capacity: 1}, $push: {players: u_id}}, {returnOriginal: false});
+    let room = result.value;//old data
     if (!room) {
       room = {hash: hash, capacity: 1, players: [u_id]};
       await db.collection("rooms").insertOne(room);
     } else {
       hash = result.value.hash;
     }
+    console.log("room_75");
+    console.log(room);
     let quarters = [false, false, false, false];
     const users = db.collection("location");
     for (let i = 0; i < room.capacity - 1; i++) {
-      const result = await users.find({u_id: room.players[i]}).toArray();//error!
+      const result = await users.find({u_id: room.players[i]}).toArray();
       if (result) {
-        if (result.x < 50) {
-          if (result.y < 50)
+        console.log("find_82");
+        console.log(result[0]);
+        if (result[0].x < 50) {
+          if (result[0].y < 50)
             quarters[0] = true;
           else
             quarters[2] = true;
         } else {
-          if (result.y < 50)
+          if (result[0].y < 50)
             quarters[1] = true;
           else
             quarters[3] = true;
         }
       }
     }
-    delta.x += Math.random() * 25 + 12.5;
-    delta.y += Math.random() * 25 + 12.5;
     if (!quarters[1]) delta.x += 50;
     else if (!quarters[2]) delta.y += 50;
     else if (!quarters[3]) {
       delta.x += 50;
       delta.y += 50;
     }
-    console.log("96");
+    console.log("103");
     console.log(delta);
   } catch (err) {
     console.log(err);
@@ -111,10 +118,10 @@ io.on("connection", socket => {
     addUser(body);
     findRoom(body.id, id).then(ans => {
       io.emit("hash of session", ans.hash);
-      console.log("114");
+      console.log("121");
       console.log(ans.delta);
       updateLocation(ans.delta).then(loc => {
-        console.log("117");
+        console.log("124");
         console.log(loc);
       });
     });
