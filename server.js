e705a0ad7127952ae2af55f41ad90eda1a866c62
yarrya <yairyabtsev@ispras.ru>
2021-09-22 18:28:55 +0300

    added rooms

--- server.js
+++ server.js
@@ -10,32 +10,25 @@ const ObjectID = mongo.ObjectID;
 
 const url = 'mongodb://localhost:27017';
 
-function addUser(body) {
+async function addUser(body) {
   const mongoClient = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
-  mongoClient.connect(function (err, client) {
-    if (err) throw err;
-    const db = client.db("codeitdb");
+  try {
+    await mongoClient.connect();
+    const db = mongoClient.db("codeitdb");
     let user = {name: body.name, u_id: body.id, score: 0, code: null};
-    db.collection("users").insertOne(user, function (err, result) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(result);
-      console.log(user);
-    });
+    await db.collection("users").insertOne(user);
     let location = {x: 0.0, y: 0.0, division: 0.0, u_id: user.u_id};
-    db.collection("location").insertOne(location, function (err, result) {
-      if (err) {
-        return console.log(err);
-      }
-      console.log(result);
-      console.log(location);
-      client.close();
-    });
-  });
+    await db.collection("location").insertOne(location);
+  } catch (err) {
+    console.log(err);
+    return false;
+  } finally {
+    await mongoClient.close();
+  }
+  return true;
 }
 
-async function updateLocation() {
+async function updateLocation(delta) {
   const mongoClient = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
   try {
     await mongoClient.connect();
@@ -45,39 +38,48 @@ async function updateLocation() {
     console.log(results);
   } catch (err) {
     console.log(err);
+    return false;
   } finally {
     await mongoClient.close();
   }
+  return true;
 }
 
-async function findRoom() {
+async function findRoom(u_id, hash) {
   const mongoClient = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
   try {
     await mongoClient.connect();
     const db = mongoClient.db("codeitdb");
     const collection = db.collection("rooms");
-    let query = {capacity: {$lt: 4}};
-    const results = await collection.find(query).toArray();
-    console.log("findRoom" + results);
+    const results = await collection.findOneAndUpdate(
+      {capacity: {$lt: 5}}, {$inc: {capacity: 1}, $push: {players: u_id}}, {new: true});
+    if (!results.value) {
+      let room = {hash: hash, capacity: 1, players: [u_id]};
+      const result = await db.collection("rooms").insertOne(room);
+    } else {
+      hash = results.value.hash;
+    }
+    // TODO: updateLocation
   } catch (err) {
     console.log(err);
+    return false;
   } finally {
     await mongoClient.close();
   }
+  return hash;
 }
 
 io.on("connection", socket => {
   const {id} = socket.client;
-  console.log(`User connected: ${id}`);
+  // console.log(`User connected: ${id}`);
   socket.emit("your id", socket.id);
   socket.on("add user", body => {
-    console.log(`${id}: ${body.name} - ${body.id}`);
+    // console.log(`${id}: ${body.name} - ${body.id}`);
     addUser(body);
-    findRoom();
-    io.emit("hash of session", "123")
+    findRoom(body.id, id).then(hash => io.emit("hash of session", hash));
   })
   socket.on("send message", body => {
-    console.log(`${id}: ${body.name} - ${body.id} - ${body.body} #${body.hash}`);
+    // console.log(`${id}: ${body.name} - ${body.id} - ${body.body} #${body.hash}`);
     io.emit("message" + body.hash, body)
 
   })
