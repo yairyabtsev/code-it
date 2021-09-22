08f40f5043645bec8eceeb210e2a6fdc7df69fe1
yarrya <yairyabtsev@ispras.ru>
2021-09-22 04:08:39 +0300

    fix

--- server.js
+++ server.js
@@ -9,34 +9,56 @@ const MongoClient = mongo.MongoClient;
 const ObjectID = mongo.ObjectID;
 
 const url = 'mongodb://localhost:27017';
-const mongoClient = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
 
 function addUser(body) {
+  const mongoClient = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
   mongoClient.connect(function (err, client) {
     if (err) throw err;
     const db = client.db("codeitdb");
-    const collection = db.collection("users");
     let user = {name: body.name, u_id: body.id, score: 0, code: null};
-    collection.insertOne(user, function (err, result) {
-
+    db.collection("users").insertOne(user, function (err, result) {
       if (err) {
         return console.log(err);
       }
       console.log(result);
       console.log(user);
+    });
+    let location = {x: 0.0, y: 0.0, division: 0.0, u_id: user.u_id};
+    db.collection("location").insertOne(location, function (err, result) {
+      if (err) {
+        return console.log(err);
+      }
+      console.log(result);
+      console.log(location);
       client.close();
     });
   });
 }
 
+async function updateLocation() {
+  const mongoClient = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
+  try {
+    await mongoClient.connect();
+    const db = mongoClient.db("codeitdb");
+    const collection = db.collection("location");
+    //...
+    console.log(results);
+  } catch (err) {
+    console.log(err);
+  } finally {
+    await mongoClient.close();
+  }
+}
+
 async function findRoom() {
+  const mongoClient = new MongoClient(url, {useNewUrlParser: true, useUnifiedTopology: true});
   try {
     await mongoClient.connect();
     const db = mongoClient.db("codeitdb");
     const collection = db.collection("rooms");
     let query = {capacity: {$lt: 4}};
     const results = await collection.find(query).toArray();
-    console.log(results);
+    console.log("findRoom" + results);
   } catch (err) {
     console.log(err);
   } finally {
@@ -51,7 +73,7 @@ io.on("connection", socket => {
   socket.on("add user", body => {
     console.log(`${id}: ${body.name} - ${body.id}`);
     addUser(body);
-    findRoom(); //TODO: get hash
+    findRoom();
     io.emit("hash of session", "123")
   })
   socket.on("send message", body => {
